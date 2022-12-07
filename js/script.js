147255abc147699e030aaee55958c1f464d249fe
Oleh Pokotyliuk <olehshmdt@gmail.com>
2022-12-07 07:55:47 +0200

    films variable

--- js/script.js
+++ js/script.js
@@ -1,3 +1,21 @@
-const isChecked = !true, isClose = false;
+let numberOfFilms = +prompt("Сколько фильмов вы просмотрели?", "3");
 
-console.log(isChecked);
+
+const personalMoviesDb = {
+    count : numberOfFilms,
+    movies : {},
+    actors : {},
+    genres : [],
+    privat : false
+};
+
+
+const a = prompt('ОДин из последних просмотренных фильмов', "");
+const b = prompt('На сколько оцените его?', "");
+const c = prompt('ОДин из последних просмотренных фильмов', "");
+const d = prompt('На сколько оцените его?', "");
+
+personalMoviesDb.movies[a] = b;
+personalMoviesDb.movies[c] = d;
+
+console.log(personalMoviesDb);
